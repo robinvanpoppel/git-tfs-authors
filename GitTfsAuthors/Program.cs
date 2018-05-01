801675c840d8dbf55ea607091f787bc9339ae520
Robin van Poppel <robin.van.poppel@cz.nl>
2018-05-01 17:17:06 +0200

    Initial add of project

---
+++ GitTfsAuthors/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.Server;
+
+namespace Konpyuta.GitTfsAuthors
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var tfsServerUri = args.FirstOrDefault();
+            Uri uri;
+            if (!Uri.TryCreate(tfsServerUri, UriKind.Absolute, out uri))
+            {
+                Console.WriteLine("Please specify a tfs server uri (e.g. http://tfs:8080/tfs)");
+                return -1;
+            }
+
+            var outFile = args.Skip(1).SingleOrDefault() ?? "authors.txt";
+            
+            CreateAuthorsFile(tfsServerUri, outFile);
+
+            return 0;
+        }
+
+        private static void CreateAuthorsFile(string tfsServerUri, string outFile)
+        {
+            using (var tfs = new TfsTeamProjectCollection(new Uri(tfsServerUri)))
+            {
+                tfs.EnsureAuthenticated();
+
+                var gss = tfs.GetService<IGroupSecurityService>();
+                var sids = gss.ReadIdentity(SearchFactor.AccountName, "Project Collection Valid Users",
+                    QueryMembership.Expanded);
+                var identities = gss.ReadIdentities(SearchFactor.Sid, sids.Members, QueryMembership.None);
+
+                using (var sw = new StreamWriter(outFile) {AutoFlush = true})
+                {
+                    foreach (var user in identities.Where(c => c != null).Where(c => c.Type == IdentityType.WindowsUser))
+                    {
+                        // Wrtie lines in the form of DISSRVTFS03\peter.pan = Peter Pan <peter.pan@disney.com>
+                        var line = $@"{user.Domain}\{user.AccountName} = {user.DisplayName} <{user.MailAddress}>";
+                        sw.WriteLine(line);
+                    }
+
+                    sw.Flush();
+                }
+            }
+        }
+    }
+}
---
+++ GitTfsAuthors/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GitTfsAuthors")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GitTfsAuthors")]
+[assembly: AssemblyCopyright("Copyright © Robin van Poppel 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("e284bd4f-880e-4c4a-86bc-5c90f1ae6d82")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
